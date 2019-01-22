52e4be1aec64b752e7edc91f398c456bffe8b9fc
Desiberio <39953109+Desiberio@users.noreply.github.com>
2019-01-22 16:54:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ client.on('message', msg => {
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-app.listen(port, host, function() {
+client.listen(port, host, function() {
   console.log("Server started.......");
 });
 
