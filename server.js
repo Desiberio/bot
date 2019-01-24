420860979d84b904ad62c2b6fa605ccfe4ee0438
Desiberio <39953109+Desiberio@users.noreply.github.com>
2019-01-24 23:21:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ client.on("message", function(message){
 	//	return;
 	//}
 	if(message.isMentioned('421897976215240705') && message.author.id == '265466662550110209'){       
-		message.reply(client.emojis.find(emoji => emoji.name === "why"));
+		message.channel.send(<:why:480099462048645170>);
 	}	
 	if(message.isMentioned('421897976215240705') && message.author.id == '265910459138441217' && !message.author.id.startsWith('265466662550110209') && !message.author.id.startsWith('259010721986248705')){
 		message.reply(qq_fortunes[Math.floor(Math.random() * qq_fortunes.length)])
